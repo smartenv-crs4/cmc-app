2c6779979ae3a7dfe710ea4988567b4eba70ba96
guido <guido.porruvecchio@gmail.com>
2017-06-27 16:32:23 +0200

    License note
    - Each file has its own license at the beginning

--- models/apps.js
+++ models/apps.js
@@ -1,10 +1,30 @@
+/*
+ ############################################################################
+ ############################### GPL III ####################################
+ ############################################################################
+ Copyright 2017 CRS4 
+ This file is part of CRS4 Microservice Core - Application (CMC-App).
+ *                                                                            *
+ CMC-Auth is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ *                                                                            *
+ CMC-Auth is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ *                                                                            *
+ You should have received a copy of the GNU General Public License
+ along with CMC-User.  If not, see <http://www.gnu.org/licenses/>.
+ * ############################################################################
+ */
+
+
 var mongoose = require('mongoose');
 var findAllFn = require('./metadata').findAll;
 var Schema = mongoose.Schema;
-
-
 var conf = require('../config').conf;
-
 
 var validateEmail = function (email) {
     var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
@@ -28,14 +48,12 @@ var appSch = conf.AppSchema || {
         notes: String
     };
 
-
 var AppSchema = new Schema(appSch, {strict: "throw"});
 
 // Static method to retrieve resource WITH metadata
 AppSchema.statics.findAll = function (conditions, fields, options, callback) {
     return findAllFn(this, 'apps', conditions, fields, options, callback);
 };
-
 
 var App = mongoose.model('App', AppSchema);
 
--- routes/jwtauth.js
+++ routes/jwtauth.js
@@ -1,4 +1,25 @@
-// @file jwtauth.js
+/*
+ ############################################################################
+ ############################### GPL III ####################################
+ ############################################################################
+ Copyright 2017 CRS4 
+ This file is part of CRS4 Microservice Core - Application (CMC-App).
+ *                                                                            *
+ CMC-App is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ *                                                                            *
+ CMC-App is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ *                                                                            *
+ You should have received a copy of the GNU General Public License
+ along with CMC-App.  If not, see <http://www.gnu.org/licenses/>.
+ * ############################################################################
+ */
+
 
 var jwt = require('jwt-simple');
 var conf = require('../config').conf;
