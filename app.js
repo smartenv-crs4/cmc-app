9c1d55679eb9be015b57539b0758d46507292916
Alessandro Romanino <a.romanino@gmail.com>
2017-06-28 10:27:23 +0200

    CMC Appms fix license header

--- app.js
+++ app.js
@@ -2,24 +2,23 @@
  ############################################################################
  ############################### GPL III ####################################
  ############################################################################
- Copyright 2017 CRS4 
- This file is part of CRS4 Microservice Core - Application (CMC-App).
- *                                                                            *
- CMC-App is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
- *                                                                            *
- CMC-App is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
- *                                                                            *
- You should have received a copy of the GNU General Public License
- along with CMC-App.  If not, see <http://www.gnu.org/licenses/>.
- * ############################################################################
+ *                  Copyright 2017 CRS4                                      *
+ *   This file is part of CRS4 Microservice Core - Application (CMC-App).   *
+ *                                                                          *
+ *     CMC-App is free software: you can redistribute it and/or modify      *
+ *   it under the terms of the GNU General Public License as published by   *
+ *    the Free Software Foundation, either version 3 of the License, or     *
+ *                (at your option) any later version.                       *
+ *                                                                          *
+ *        CMC-App is distributed in the hope that it will be useful,        *
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of      *
+ *       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the      *
+ *              GNU General Public License for more details.                *
+ *                                                                          *
+ *     You should have received a copy of the GNU General Public License    *
+ *     along with CMC-App.  If not, see <http://www.gnu.org/licenses/>.     *
+ ############################################################################
  */
-
 
 var express = require('express');
 var path = require('path');
